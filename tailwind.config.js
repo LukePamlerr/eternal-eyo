9583b07ee34f593cad068f1c57e0c4974cf67a74
Eyo <donovanbrady102@gmail.com>
2025-07-17 03:53:33 -0400

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './app/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        black: '#1C2526',
+        'light-green': '#98FF98',
+      },
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+      },
+      animation: {
+        'pulse-slow': 'pulse 4s cubic-bezier(0.4, 0, 0.6, 1) infinite',
+      },
+    },
+  },
+  plugins: [require('@tailwindcss/typography')],
+}
